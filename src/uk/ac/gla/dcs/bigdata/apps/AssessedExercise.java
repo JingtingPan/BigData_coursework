6343c3a117434c3f5351a3b677a7f2b1bfb37552
Beijia Kan (student) <2876154K@student.gla.ac.uk>
2024-02-18 14:17:42 +0000

    code updates

--- src/uk/ac/gla/dcs/bigdata/apps/AssessedExercise.java
+++ src/uk/ac/gla/dcs/bigdata/apps/AssessedExercise.java
@@ -1,107 +1,112 @@
 package uk.ac.gla.dcs.bigdata.apps;
 
 import java.io.File;
-import java.util.List;
+import java.util.*;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.*;
+import org.apache.spark.broadcast.Broadcast;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
-import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-
+import scala.Tuple2;
 import uk.ac.gla.dcs.bigdata.providedfunctions.NewsFormaterMap;
 import uk.ac.gla.dcs.bigdata.providedfunctions.QueryFormaterMap;
-import uk.ac.gla.dcs.bigdata.providedstructures.DocumentRanking;
-import uk.ac.gla.dcs.bigdata.providedstructures.NewsArticle;
-import uk.ac.gla.dcs.bigdata.providedstructures.Query;
+import uk.ac.gla.dcs.bigdata.providedstructures.*;
+import uk.ac.gla.dcs.bigdata.providedutilities.DPHScorer;
 
-/**
- * This is the main class where your Spark topology should be specified.
- * 
- * By default, running this class will execute the topology defined in the
- * rankDocuments() method in local mode, although this may be overriden by
- * the spark.master environment variable.
- * @author Richard
- *
- */
 public class AssessedExercise {
 
-	
 	public static void main(String[] args) {
-		
-		File hadoopDIR = new File("resources/hadoop/"); // represent the hadoop directory as a Java file so we can get an absolute path for it
-		System.setProperty("hadoop.home.dir", hadoopDIR.getAbsolutePath()); // set the JVM system property so that Spark finds it
-		
-		// The code submitted for the assessed exerise may be run in either local or remote modes
-		// Configuration of this will be performed based on an environment variable
-		String sparkMasterDef = System.getenv("spark.master");
-		if (sparkMasterDef==null) sparkMasterDef = "local[2]"; // default is local mode with two executors
-		
-		String sparkSessionName = "BigDataAE"; // give the session a name
-		
-		// Create the Spark Configuration 
-		SparkConf conf = new SparkConf()
-				.setMaster(sparkMasterDef)
-				.setAppName(sparkSessionName);
-		
-		// Create the spark session
-		SparkSession spark = SparkSession
-				  .builder()
-				  .config(conf)
-				  .getOrCreate();
-	
-		
-		// Get the location of the input queries
-		String queryFile = System.getenv("bigdata.queries");
-		if (queryFile==null) queryFile = "data/queries.list"; // default is a sample with 3 queries
-		
-		// Get the location of the input news articles
-		String newsFile = System.getenv("bigdata.news");
-		if (newsFile==null) newsFile = "data/TREC_Washington_Post_collection.v3.example.json"; // default is a sample of 5000 news articles
-		
-		// Call the student's code
+
+		File hadoopDIR = new File("resources/hadoop/");
+		System.setProperty("hadoop.home.dir", hadoopDIR.getAbsolutePath());
+
+		SparkConf conf = new SparkConf().setAppName("BigDataAE").setMaster("local[*]");
+		SparkSession spark = SparkSession.builder().config(conf).getOrCreate();
+
+		String queryFile = "data/queries.list";
+		String newsFile = "data/TREC_Washington_Post_collection.v3.example.json";
+
 		List<DocumentRanking> results = rankDocuments(spark, queryFile, newsFile);
-		
-		// Close the spark session
+
 		spark.close();
-		
-		// Check if the code returned any results
-		if (results==null) System.err.println("Topology return no rankings, student code may not be implemented, skiping final write.");
-		else {
-			
-			// We have set of output rankings, lets write to disk
-			
-			// Create a new folder 
-			File outDirectory = new File("results/"+System.currentTimeMillis());
-			if (!outDirectory.exists()) outDirectory.mkdir();
-			
-			// Write the ranking for each query as a new file
-			for (DocumentRanking rankingForQuery : results) {
-				rankingForQuery.write(outDirectory.getAbsolutePath());
-			}
+
+		if (results != null) {
+			File outDirectory = new File("results/" + System.currentTimeMillis());
+			outDirectory.mkdirs();
+			results.forEach(ranking -> ranking.write(outDirectory.getAbsolutePath()));
 		}
-		
-		
 	}
-	
-	
-	
+
 	public static List<DocumentRanking> rankDocuments(SparkSession spark, String queryFile, String newsFile) {
-		
-		// Load queries and news articles
-		Dataset<Row> queriesjson = spark.read().text(queryFile);
-		Dataset<Row> newsjson = spark.read().text(newsFile); // read in files as string rows, one row per article
-		
-		// Perform an initial conversion from Dataset<Row> to Query and NewsArticle Java objects
-		Dataset<Query> queries = queriesjson.map(new QueryFormaterMap(), Encoders.bean(Query.class)); // this converts each row into a Query
-		Dataset<NewsArticle> news = newsjson.map(new NewsFormaterMap(), Encoders.bean(NewsArticle.class)); // this converts each row into a NewsArticle
-		
-		//----------------------------------------------------------------
-		// Your Spark Topology should be defined here
-		//----------------------------------------------------------------
-		
-		
-		return null; // replace this with the the list of DocumentRanking output by your topology
+		Dataset<Query> queries = spark.read().text(queryFile).map(new QueryFormaterMap(), Encoders.bean(Query.class));
+		Dataset<NewsArticle> news = spark.read().text(newsFile).map(new NewsFormaterMap(), Encoders.bean(NewsArticle.class));
+
+		// Calculate average document length and total term frequencies
+		// Correctly broadcasting values without Encoders
+		Broadcast<Double> avgDocLength = spark.sparkContext().broadcast(calculateAverageDocumentLength(news), scala.reflect.ClassTag$.MODULE$.apply(Double.class));
+		Broadcast<Map<String, Long>> totalTermFreqs = spark.sparkContext().broadcast(calculateTotalTermFrequencies(news), scala.reflect.ClassTag$.MODULE$.apply(Map.class));
+
+		JavaPairRDD<Query, NewsArticle> queryArticlePairs = queries.toJavaRDD().cartesian(news.toJavaRDD());
+
+		JavaRDD<DocumentRanking> documentRankings = queryArticlePairs.mapPartitionsToPair(new PairFlatMapFunction<Iterator<Tuple2<Query, NewsArticle>>, Query, RankedResult>() {
+			@Override
+			public Iterator<Tuple2<Query, RankedResult>> call(Iterator<Tuple2<Query, NewsArticle>> input) {
+				List<Tuple2<Query, RankedResult>> results = new ArrayList<>();
+				while (input.hasNext()) {
+					Tuple2<Query, NewsArticle> pair = input.next();
+					Query query = pair._1();
+					NewsArticle article = pair._2();
+					double score = 0.0;
+					for (String term : query.getQueryTerms()) {
+						long tf = article.getProcessedContents().stream().filter(t -> t.equals(term)).count();
+						long df = totalTermFreqs.getValue().getOrDefault(term, 0L);
+						// Correcting the method call to match the expected parameter types.
+						double dphScore = DPHScorer.getDPHScore(
+								(short) tf,
+								(int) df,  // Cast total term frequency (df) to int as required
+								article.getProcessedContents().size(),
+								avgDocLength.getValue(),
+								totalTermFreqs.getValue().size() // Use the map size for the total number of unique terms, which might not be correct. Adjust as needed.
+						);
+						score += dphScore;
+					}
+					score /= query.getQueryTerms().size();
+					results.add(new Tuple2<>(query, new RankedResult(article.getId(), article, score)));
+				}
+				return results.iterator();
+			}
+		}).groupByKey().mapToPair(tuple -> {
+			Query query = tuple._1();
+			List<RankedResult> rankedResults = new ArrayList<>();
+			tuple._2().forEach(rankedResults::add);
+			rankedResults.sort(Comparator.comparingDouble(RankedResult::getScore).reversed());
+			return new Tuple2<>(query, rankedResults.subList(0, Math.min(10, rankedResults.size())));
+		}).flatMap(tuple -> {
+			List<DocumentRanking> rankings = new ArrayList<>();
+			Query query = tuple._1();
+			List<RankedResult> rankedResults = tuple._2();
+			rankings.add(new DocumentRanking(query, rankedResults));
+			return rankings.iterator();
+		});
+
+		return documentRankings.collect();
 	}
-	
-	
+
+	private static double calculateAverageDocumentLength(Dataset<NewsArticle> news) {
+		JavaRDD<NewsArticle> newsRDD = news.toJavaRDD();
+		double totalLength = newsRDD.mapToDouble(article -> article.getProcessedContents().size()).sum();
+		long docCount = newsRDD.count();
+		return totalLength / docCount;
+	}
+
+	private static Map<String, Long> calculateTotalTermFrequencies(Dataset<NewsArticle> news) {
+		JavaRDD<NewsArticle> newsRDD = news.toJavaRDD();
+		return newsRDD.flatMap(article -> Arrays.asList(article.getProcessedContents().toArray(new String[0])).iterator())
+				.mapToPair(term -> new Tuple2<>(term, 1L))
+				.reduceByKey(Long::sum)
+				.collectAsMap();
+	}
 }
